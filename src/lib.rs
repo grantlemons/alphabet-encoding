9f7b9ce86b716589f7ff8765bb4e26ab7492c2e6
Grant Lemons <grantlemons@aol.com>
2025-03-04 00:03:48 -0700

    removed dependencies and converted to library

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use anyhow::{Context, Result};
+use std::fmt::Display;
+use std::num::ParseIntError;
 use std::str::FromStr;
 
 fn should_encode(c: char) -> bool {
@@ -6,21 +7,19 @@ fn should_encode(c: char) -> bool {
 }
 
 struct EncodedChar(char);
-impl ToString for EncodedChar {
-    fn to_string(&self) -> String {
-        format!("x{:02x}", self.0 as u8)
+impl Display for EncodedChar {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "x{:02x}", self.0 as u8)
     }
 }
 
 impl FromStr for EncodedChar {
-    type Err = anyhow::Error;
+    type Err = ParseIntError;
 
     fn from_str(str: &str) -> Result<Self, Self::Err> {
         let str_chars: Vec<char> = str.chars().collect();
         let chars: String = str_chars[1..=2].iter().collect();
-        let c = u8::from_str_radix(&chars, 16)
-            .with_context(|| format!("Unable to convert `{}` to u8", &chars))?
-            as char;
+        let c = u8::from_str_radix(&chars, 16)? as char;
 
         Ok(Self(c))
     }
@@ -38,12 +37,12 @@ pub fn encode(str: String) -> String {
         .collect()
 }
 
-pub fn decode(str: String) -> Result<String> {
+pub fn decode(str: String) -> Result<String, ParseIntError> {
     let str_chars: Vec<char> = str.chars().collect();
 
     Ok(str
         .match_indices('x')
-        .map(|(i, _)| -> Result<(String, String)> {
+        .map(|(i, _)| -> Result<(String, String), ParseIntError> {
             let chars: String = str_chars[i..=i + 2].iter().collect();
             let encoded = EncodedChar::from_str(&chars)?;
             Ok((chars, encoded.0.to_string()))
